acac165a9baf8b5527c4f30b48a6600b6f3f99f2
Max Drexel <lord_onager@yahoo.com>
2017-02-16 19:43:34 -0500

    Started adding multi-parameter arguments.

--- CommandLineArguments/Arguments/ArgumentAttribute.cs
+++ CommandLineArguments/Arguments/ArgumentAttribute.cs
@@ -19,18 +19,25 @@ namespace Arguments
         /// <param name="shortName">The short name by which to expose the argument; for example, t.</param>
         /// <param name="exampleValue">An example of a valid value the argument could be.</param>
         /// <param name="description">A brief description of what the argument does or is used for.</param>
+        /// <param name="position">
+        /// The argument's position, 0 indexed. Set to -1 if the argument does not require a command line value.
+        /// Defaults to 0. For example, given "-Arguments A B C", A is position 0, B is position 1, etc. 
+        /// If no command line value is required, the argument would take the form of "-Argument".
+        /// </param>
         public ArgumentAttribute(
             string defaultValue, 
             string longName, 
             string shortName,
             string exampleValue,
-            string description)
+            string description,
+            int position = 0)
         {
             this.DefaultValue = defaultValue;
             this.ExampleValue = exampleValue;
             this.LongName = longName;
             this.ShortName = shortName;
             this.Description = description;
+            this.Position = position;
         }
 
         /// <summary>
@@ -75,5 +82,16 @@ namespace Arguments
         /// "Controls the cache refresh interval (seconds)".
         /// </example>
         public string Description { get; private set; }
+
+        /// <summary>
+        /// When multiple <see cref="ArgumentAttribute"/>s with the same long or short name exist, the 0-indexed 
+        /// location within the supplied arguments list the field should take the value of. A position of -1 means
+        /// the attribute does not take a user-supplied argument.
+        /// </summary>
+        /// <example>
+        /// For an example argument "--Argument A B C", the position of A is 0, B is 1, and C is 2. Or, if the usage
+        /// is "--Argument", the position is -1.
+        /// </example>
+        public int Position { get; private set; }
     }
 }
---
+++ CommandLineArguments/Arguments/ArgumentDictionary.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arguments
+{
+    internal class ArgumentDictionary : IDictionary<string, AttributeField[]>
+    {
+        public AttributeField[] this[string key]
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+
+        public ICollection<string> Keys
+        {
+            get
+            {
+
+            }
+        }
+
+        public ICollection<AttributeField[]> Values => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly { get; private set; }
+
+        private Dictionary<string, AttributeField[]> innerDictionary;
+
+        public void Add(string key, AttributeField[] value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(KeyValuePair<string, AttributeField[]> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<string, AttributeField[]> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<string, AttributeField[]>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<string, AttributeField[]>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<string, AttributeField[]> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(string key, out AttributeField[] value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
