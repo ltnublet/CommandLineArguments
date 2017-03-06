6e8fa30a31c82723d992b876fc91c3cde74a11fb
ltnublet <lord_onager@yahoo.com>
2017-03-05 20:27:17 -0500

    Removed HeapWalker (can't get it to work), added some xmldocs, and changed stylecop to allow hungarian notation.

--- CommandLineArguments/Arguments/Collections/Tree.cs
+++ CommandLineArguments/Arguments/Collections/Tree.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace Arguments.Collections
 {
+    /// <summary>
+    /// Represents a tree of <see cref="TreeNode{T}"/>s.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="TreeNode{T}"/> contained by the tree.</typeparam>
     internal class Tree<T> : IEnumerable<T>
     {
+        /// <summary>
+        /// Instantiates a new <see cref="Tree{T}"/> using the supplied <see cref="TreeNode{T}"/> as the root.
+        /// </summary>
+        /// <param name="root">The root of the tree. Must be non-null.</param>
         public Tree(TreeNode<T> root)
         {
             if (root == null)
@@ -19,13 +27,24 @@ namespace Arguments.Collections
             this.Root = root;
         }
 
+        /// <summary>
+        /// The root of the tree.
+        /// </summary>
         public TreeNode<T> Root { get; private set; }
 
+        /// <summary>
+        /// Returns an <see cref="IEnumerator{out T}"/> which performs a depth-first flattening of the associated <see cref="Tree{T}"/>.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator{T}"/> where the type is determined by the <see cref="Tree{T}"/>.</returns>
         public IEnumerator<T> GetEnumerator()
         {
             return this.Root.GetEnumerator();
         }
 
+        /// <summary>
+        /// Returns an <see cref="IEnumerator{out T}"/> which performs a depth-first flattening of the associated <see cref="Tree{T}"/>.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator{T}"/> where the type is determined by the <see cref="Tree{T}"/>.</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
