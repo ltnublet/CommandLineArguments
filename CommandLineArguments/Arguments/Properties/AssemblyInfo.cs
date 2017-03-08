05892727562e1b838da5bd2c4f133cd7947e3d8e
Max Drexel <lord_onager@yahoo.com>
2017-03-08 00:09:22 -0500

    Changed assembly name and bumped version

--- CommandLineArguments/Arguments/Properties/AssemblyInfo.cs
+++ CommandLineArguments/Arguments/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]
 
 // Add Tests project to list of assemblies which can access internals:
 [assembly: InternalsVisibleTo("Arguments.Tests")]
